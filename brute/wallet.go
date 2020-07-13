4c7b5f32a6fc7a4b99b1937b85c4133adbe9ea31
Alex <itoutsourcing86@gmail.com>
2020-07-13 16:45:30 +0500

    refactoring

--- brute/wallet.go
+++ brute/wallet.go
@@ -22,6 +22,8 @@ type wallet struct {
 	balanceUnconfirmedU uint64
 	balanceConfirmedC   uint64
 	balanceUnconfirmedC uint64
+	// client              *electrum.Client
+	tx *[]electrum.Tx
 }
 
 func newWallet(HEX string) *wallet {
@@ -34,33 +36,42 @@ func newWallet(HEX string) *wallet {
 }
 
 func (w *wallet) checkBalance(client *electrum.Client, out string) {
-	balanceUncompressed, err := client.AddressBalance(w.addressCompressed)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.balanceConfirmedU = balanceUncompressed.Confirmed
-	w.balanceUnconfirmedU = balanceUncompressed.Unconfirmed
-
-	if balanceUncompressed.Confirmed != 0 && balanceUncompressed.Unconfirmed != 0 {
-		saveData(w.String(), out)
-	}
-
-	balanceCompressed, err := client.AddressBalance(w.addressUncompressed)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	w.balanceConfirmedC = balanceCompressed.Confirmed
-	w.balanceUnconfirmedC = balanceCompressed.Unconfirmed
-
-	if balanceCompressed.Confirmed != 0 && balanceCompressed.Unconfirmed != 0 {
+	w.getTransactions(client)
+	w.balanceCompressed(client)
+	w.balanceUncompressed(client)
+	if w.balanceConfirmedU != 0 || w.balanceUnconfirmedU != 0 || w.balanceConfirmedC != 0 || w.balanceUnconfirmedC != 0 || len(*w.tx) != 0 {
 		saveData(w.String(), out)
 	}
 }
 
+func (w *wallet) balanceUncompressed(client *electrum.Client) {
+	balanceUncompressed, err := client.AddressBalance(w.addressUncompressed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.balanceConfirmedU = balanceUncompressed.Confirmed
+	w.balanceUnconfirmedU = balanceUncompressed.Unconfirmed
+}
+
+func (w *wallet) balanceCompressed(client *electrum.Client) {
+	balanceCompressed, err := client.AddressBalance(w.addressCompressed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.balanceConfirmedC = balanceCompressed.Confirmed
+	w.balanceUnconfirmedC = balanceCompressed.Unconfirmed
+}
+
+func (w *wallet) getTransactions(client *electrum.Client) {
+	tx, err := client.AddressHistory(w.addressUncompressed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.tx = tx
+}
+
 func (w *wallet) String() string {
-	return fmt.Sprintf("HEX: %s\nWIF: %s\nAddress uncompressed: %s\nAddress compressed: %s\nBalance confirmed uncompressed: %d\nBalance unconfirmed uncompressed: %d\nBalance confirmed compress: %d\nBalance unconfirmed compress: %d\n",
-		w.HEX, w.WIF, w.addressUncompressed, w.addressCompressed, w.balanceConfirmedU, w.balanceUnconfirmedU, w.balanceConfirmedC, w.balanceUnconfirmedC,
+	return fmt.Sprintf("HEX: %s\nWIF: %s\nAddress uncompressed: %s\nAddress compressed: %s\nBalance confirmed uncompressed: %d\nBalance unconfirmed uncompressed: %d\nBalance confirmed compress: %d\nBalance unconfirmed compress: %d\nTx: %v\n",
+		w.HEX, w.WIF, w.addressUncompressed, w.addressCompressed, w.balanceConfirmedU, w.balanceUnconfirmedU, w.balanceConfirmedC, w.balanceUnconfirmedC, w.tx,
 	)
 }
